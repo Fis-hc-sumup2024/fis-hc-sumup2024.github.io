e884a1705e4aa43c5338ab4bcfacb87e1ff60c0e
TuTC9 <tutc9@fpt.com>
2025-01-05 01:43:54 +0700

    build(app):  update loading

--- src/hooks/useLoading.ts
+++ src/hooks/useLoading.ts
@@ -4,18 +4,8 @@ export const useLoading = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const handlePageLoad = () => {
+    window.onload = () => {
       setIsLoading(false);
-    };
-
-    if (document.readyState === "complete") {
-      handlePageLoad();
-    } else {
-      window.addEventListener("load", handlePageLoad);
-    }
-
-    return () => {
-      window.removeEventListener("load", handlePageLoad);
     };
   }, []);
 
