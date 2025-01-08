7aacc1855c5d6d8f5a326e491e77f198563f27f3
TuTC9 <tutc9@fpt.com>
2025-01-08 17:07:10 +0700

    build(app): update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,54 +38,60 @@ const Home: React.FC<HomeProps> = ({ data }) => {
             className="bg-white rounded-full border-[3px] border-[#72E2F0]"
           />
         </div>
-        <h2 className="font-semibold">Chào {data?.accountDisplay}</h2>
-        <p>Số may mắn của bạn là</p>
-        <p
-          style={{
-            textShadow: "0px 1px 1px 0px #00000040",
-            lineHeight: innerHeight > 750 ? "54.4px" : "36px",
-          }}
-          className={`${
-            isManager ? "text-[#E20000]" : "text-[#72E2F0]"
-          } text-xl text-center items-center`}
-        >
-          {isManager ? "#$&?" : data?.code}
-        </p>
+        {isManager ? (
+          <>
+            <div className="text-center">
+              <h2 className="font-semibold">Chào {data?.accountDisplay}</h2>
+              <p>
+                Chào mừng đến với trạm không gian Công dân tương lai cùng
+                Healthcare.
+              </p>
+            </div>
+            <p className="text-center">
+              Mời bạn di chuyển đến{" "}
+              <span className="font-bold">Cụm Công Dân Care Connectors</span> để
+              tham dự bữa tiệc nhé!
+            </p>
+          </>
+        ) : (
+          <>
+            <h2 className="font-semibold">Chào {data?.accountDisplay}</h2>
+            <p>Số may mắn của bạn là</p>
+            <p
+              style={{
+                textShadow: "0px 1px 1px 0px #00000040",
+                lineHeight: innerHeight > 750 ? "54.4px" : "36px",
+              }}
+              className={`${
+                isManager ? "text-[#E20000]" : "text-[#72E2F0]"
+              } text-xl text-center items-center`}
+            >
+              {isManager ? "#$&?" : data?.code}
+            </p>
+          </>
+        )}
       </div>
       <div className="flex flex-col items-center">
         <div className="flex">
-          <RobotAnswer
-            className={`${isManager ? "ml-[35px]" : ""} mb-[-55px] z-10`}
-          />
-          {isManager && <Warning className="ml-[-40px] z-10" />}
+          <RobotAnswer className="ml-[35px] mb-[-55px] z-10" />
+          <Warning className="ml-[-40px] z-10" />
         </div>
         <div className="gap-6 pt-10 pb-8 px-4 w-full bg-white-blur-15 backdrop-blur-[20px] rounded-[20px] text-center border border-white-blur-15">
-          {isManager ? (
-            <p>
-              Chào mừng đến với trạm không gian Công dân tương lai cùng
-              Healthcare
-            </p>
-          ) : (
-            <React.Fragment>
-              <p>
-                Năm 2024 Dr.AI chẩn đoán bạn bị{" "}
-                <span className="font-bold">
-                  "{diagnosCategory?.diagnosName}"
-                </span>
-              </p>
-              {diagnosItem &&
-                Object.keys(diagnosItem?.treatment).map(
-                  (key: string, index: number) => (
-                    <p key={`${key}-${index}`}>
-                      {key}:{" "}
-                      <span className="font-bold">
-                        {(diagnosItem?.treatment as Treatment)[key] ?? ""}
-                      </span>
-                    </p>
-                  )
-                )}
-            </React.Fragment>
-          )}
+          <p>
+            Năm 2024 Dr.AI chẩn đoán bạn bị{" "}
+            <span className="font-bold">"{diagnosCategory?.diagnosName}"</span>
+          </p>
+          {diagnosItem &&
+            Object.keys(diagnosItem?.treatment).map(
+              (key: string, index: number) => (
+                <p key={`${key}-${index}`}>
+                  {key}:{" "}
+                  <span className="font-bold">
+                    {(diagnosItem?.treatment as Treatment)[key] ?? ""}
+                  </span>
+                </p>
+              )
+            )}
         </div>
       </div>
     </div>
