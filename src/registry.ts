ef1b6bb914e7e79f26dde31d753cb075a5e110f8
TuTC9 <tutc9@fpt.com>
2025-01-16 14:46:17 +0700

    build(app): final HN

--- src/registry.ts
+++ src/registry.ts
@@ -1,9 +1,12 @@
 import AnhDTL28 from "./assets/AnhDTL28.jpeg";
+import AnhLT152 from "./assets/AnhLT152.jpeg";
 import AnhLV27 from "./assets/AnhLV27.jpeg";
 import AnhPT150 from "./assets/AnhPT150.jpeg";
 import AnhVN5 from "./assets/AnhVN5.jpeg";
+import CanhNC from "./assets/CanhNC.jpeg";
 import ChinhNV6 from "./assets/ChinhNV6.jpeg";
 import CuongNV124 from "./assets/CuongNV124.jpeg";
+import DangVX from "./assets/DangVX.jpeg";
 import DatLT35 from "./assets/DatLT35.jpeg";
 import DatLT99 from "./assets/DatLT99.jpeg";
 import DauMT from "./assets/DauMT.jpeg";
@@ -13,12 +16,15 @@ import DungDV25 from "./assets/DungDV25.jpeg";
 import DungTQ6 from "./assets/DungTQ6.jpeg";
 import DuyDQ45 from "./assets/DuyDQ45.jpeg";
 import DuyTK8 from "./assets/DuyTK8.jpeg";
+import HoaNH10 from "./assets/HoaNH10.jpeg";
 import HungNQ64 from "./assets/HungNQ64.jpeg";
 import HungPG from "./assets/HungPG.jpeg";
 import HungPT42 from "./assets/HungPT42.jpeg";
+import HungVV13 from "./assets/HungVV13.jpeg";
 import HuyenDT48 from "./assets/HuyenDT48.jpeg";
 import HuyNQD from "./assets/HuyNQD.jpeg";
 import HuyVD18 from "./assets/HuyVD18.jpeg";
+import LanHDT from "./assets/LanHDT.jpeg";
 import LeLTB from "./assets/LeLTB.jpeg";
 import LinhVN11 from "./assets/LinhVN11.jpeg";
 import LinhVTN5 from "./assets/LinhVTN5.jpeg";
@@ -30,25 +36,37 @@ import NgoanNT11 from "./assets/NgoanNT11.jpeg";
 import PhongNH34 from "./assets/PhongNH34.jpeg";
 import PhongVH12 from "./assets/PhongVH12.jpeg";
 import PhuongDTM18 from "./assets/PhuongDTM18.jpeg";
+import QuanNA34 from "./assets/QuanNA34.jpeg";
+import QuanNH72 from "./assets/QuanNH72.jpeg";
+import QuyTP5 from "./assets/QuyTP5.jpeg";
 import SonHT7 from "./assets/SonHT7.jpeg";
 import TanDN14 from "./assets/TanDN14.jpeg";
+import TanNN10 from "./assets/TanNN10.jpeg";
+import ThangPN from "./assets/ThangPN.jpeg";
+import ThucLV4 from "./assets/ThucLV4.jpeg";
 import TienNC8 from "./assets/TienNC8.jpeg";
 import TrangDT45 from "./assets/TrangDT45.jpeg";
 import TrangVT33 from "./assets/TrangVT33.jpeg";
+import TranT25 from "./assets/TranT25.jpeg";
+import TrungNT67 from "./assets/TrungNT67.jpeg";
 import TruongLD7 from "./assets/TruongLD7.jpeg";
 import TungPT from "./assets/TungPT.jpeg";
 import TungT2 from "./assets/TungT2.jpeg";
 import TuVQ2 from "./assets/TuVQ2.jpeg";
 import VanNT84 from "./assets/VanNT84.jpeg";
 import VuNC3 from "./assets/VuNC3.jpeg";
+import YenLTH15 from "./assets/YenLTH15.jpeg";
 
 export const hnAvatars = [
   { account: "anhdtl28", avatar: AnhDTL28 },
+  { account: "anhlt152", avatar: AnhLT152 },
   { account: "anhlv27", avatar: AnhLV27 },
   { account: "anhpt150", avatar: AnhPT150 },
   { account: "anhvn5", avatar: AnhVN5 },
+  { account: "canhnc", avatar: CanhNC },
   { account: "chinhnv6", avatar: ChinhNV6 },
   { account: "cuongnv124", avatar: CuongNV124 },
+  { account: "dangvx", avatar: DangVX },
   { account: "datlt35", avatar: DatLT35 },
   { account: "datlt99", avatar: DatLT99 },
   { account: "daumt", avatar: DauMT },
@@ -58,12 +76,15 @@ export const hnAvatars = [
   { account: "dungtq6", avatar: DungTQ6 },
   { account: "duydq45", avatar: DuyDQ45 },
   { account: "duytk8", avatar: DuyTK8 },
+  { account: "hoanh10", avatar: HoaNH10 },
   { account: "hungnq64", avatar: HungNQ64 },
   { account: "hungpg", avatar: HungPG },
   { account: "hungpt42", avatar: HungPT42 },
+  { account: "hungvv13", avatar: HungVV13 },
   { account: "huyendt48", avatar: HuyenDT48 },
   { account: "huynqd", avatar: HuyNQD },
   { account: "huyvd18", avatar: HuyVD18 },
+  { account: "lanhdt", avatar: LanHDT },
   { account: "leltb", avatar: LeLTB },
   { account: "linhvn11", avatar: LinhVN11 },
   { account: "linhvtn5", avatar: LinhVTN5 },
@@ -75,15 +96,24 @@ export const hnAvatars = [
   { account: "phongnh34", avatar: PhongNH34 },
   { account: "phongvh12", avatar: PhongVH12 },
   { account: "phuongdtm18", avatar: PhuongDTM18 },
+  { account: "quanna34", avatar: QuanNA34 },
+  { account: "quannh72", avatar: QuanNH72 },
+  { account: "quytp5", avatar: QuyTP5 },
   { account: "sonht7", avatar: SonHT7 },
   { account: "tandn14", avatar: TanDN14 },
+  { account: "tannn10", avatar: TanNN10 },
+  { account: "thangpn", avatar: ThangPN },
+  { account: "thuclv4", avatar: ThucLV4 },
   { account: "tiennc8", avatar: TienNC8 },
   { account: "trangdt45", avatar: TrangDT45 },
   { account: "trangvt33", avatar: TrangVT33 },
+  { account: "trant25", avatar: TranT25 },
+  { account: "trungnt67", avatar: TrungNT67 },
   { account: "truongld7", avatar: TruongLD7 },
   { account: "tungpt", avatar: TungPT },
   { account: "tungt2", avatar: TungT2 },
   { account: "tuvq2", avatar: TuVQ2 },
   { account: "vannt84", avatar: VanNT84 },
   { account: "vunc3", avatar: VuNC3 },
+  { account: "yenlth15", avatar: YenLTH15 },
 ];
